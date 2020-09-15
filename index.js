142ab016544231683d2f648e9d0e8bb9355d56d0
makand <makand1989@gmail.com>
2020-09-15 12:23:13 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const alegroScraper = require('./scraper-helper/alegro.puppeteer');
+const compression = require('compression');
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded());
+app.use(express.json());
+app.use(compression());
+app.get('/', (req, res) => {
+    res.send("Alegro parsing is working;");
+});
+app.post('/scraper', async (req, res) => {
+    if(req.body.url){
+      const bodyHtml = await alegroScraper(req.body.url);
+      return res.send(bodyHtml);
+    }else {
+        return res.send('No url param in request');
+    }
+
+});
+app.listen(process.env.PORT || 5000);
